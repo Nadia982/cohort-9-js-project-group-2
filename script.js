52a02840217870a9e1da2068d781d400119f4b31
Natalie <79932225+Nadia982@users.noreply.github.com>
2022-08-16 14:41:50 +0100

    updated HTML and CSS, added array of quotes to JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const quotes = [
+    "We know who we are, but not what we may be",
+    "This above all: to thine own self be true.",
+    "All's well that ends well.",
+    "Go wisely and slowly. Those who rush stumble and fall.",
+    "To be or not to be, that is the question.",
+    "Summer's lease hat all too short a date",
+    "Time travels at different speeds for different people. I can tell you who time strolls for, who it trots for, who it gallops for, and who it stops cold for.",
+    "All that glisters is not gold, Often have you heard that told..."
+]
